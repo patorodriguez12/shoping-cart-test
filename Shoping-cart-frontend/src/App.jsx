4d124d7689393f12c3d332aed32a84cb09bbb43d
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-09-30 21:18:33 -0300

    ✨ feat(routes): created initial routes

--- Shoping-cart-frontend/src/App.jsx
+++ Shoping-cart-frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
 function App() {
   return (
-    <>
-    test commit
-    </>
-  )
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
