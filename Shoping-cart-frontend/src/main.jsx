3de902701d53e37545499db8ba71214ed60aa114
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-10-01 14:56:22 -0300

    ✨ feat(productsSlice): implemented action for fetching products data

--- Shoping-cart-frontend/src/main.jsx
+++ Shoping-cart-frontend/src/main.jsx
@@ -6,13 +6,16 @@ import App from "./App.jsx";
 import NavBar from "./components/NavBar.jsx";
 import "./index.css";
 import store from "./app/store.js";
+import { productsFetch } from "./features/productsSlice.js";
+
+store.dispatch(productsFetch())
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <StrictMode>
       <BrowserRouter>
         <NavBar />
-        <App />
+        <App /> 
       </BrowserRouter>
     </StrictMode>
   </Provider>
