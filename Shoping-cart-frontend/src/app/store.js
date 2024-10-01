935bbca06135fc06f77e28c33d82733e5d8b860e
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-10-01 19:00:51 -0300

    ✨ feat(cart): cart add implementation, cardSlice created

--- Shoping-cart-frontend/src/app/store.js
+++ Shoping-cart-frontend/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { productsApi } from "../features/productsApi.js";
+import cartReducer from "../features/cartSlice.js";
 
 const store = configureStore({
   reducer: {
+    cart: cartReducer,
     [productsApi.reducerPath]: productsApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
--- Shoping-cart-frontend/src/components/Home.jsx
+++ Shoping-cart-frontend/src/components/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { useGetAllProductsQuery } from "../features/productsApi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../features/cartSlice";
 
 function Home() {
   const { data, error, isLoading } = useGetAllProductsQuery();
+  const dispatch = useDispatch();
+
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product));
+  };
 
   return (
     <div className="p-8 px-16">
@@ -15,14 +22,28 @@ function Home() {
           <h2 className="text-[40px] font-normal text-center">New Arrivals</h2>
           <div className="flex flex-wrap justify-between mt-8">
             {data?.map((product) => (
-              <div key={product.id} className="w-[250px] max-w-full  flex flex-col justify-between my-8 mx-auto p-4 shadow-2xl rounded-xl">
+              <div
+                key={product.id}
+                className="w-[250px] max-w-full  flex flex-col justify-between my-8 mx-auto p-4 shadow-2xl rounded-xl"
+              >
                 <h3 className="text-[25px] font-normal">{product.name}</h3>
-                <img src={product.image} alt={product.name} className="w-4/5 mt-4 mx-auto"/>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="w-4/5 mt-4 mx-auto"
+                />
                 <div className="flex flex-col justify-between items-center">
                   <span>{product.description}</span>
-                  <span className="text-[20px] font-bold">${product.price}</span>
+                  <span className="text-[20px] font-bold">
+                    ${product.price}
+                  </span>
                 </div>
-                <button className="w-full h-[40px] rounded-xl mt-8 font-normal bg-[#4b70e2] text-white tracking-wide">Add To Cart</button>
+                <button
+                  onClick={() => handleAddToCart(product)}
+                  className="w-full h-[40px] rounded-xl mt-8 font-normal bg-[#4b70e2] text-white tracking-wide"
+                >
+                  Add To Cart
+                </button>
               </div>
             ))}
           </div>
