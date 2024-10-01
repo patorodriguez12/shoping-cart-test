6b82d823ff86ad343fd28fc8053aa5a3ca3e2b94
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-10-01 10:19:29 -0300

    💄 style(navbar): first basic styling

--- Shoping-cart-frontend/src/App.jsx
+++ Shoping-cart-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
+import "./App.css";
 
 function App() {
   return (
--- Shoping-cart-frontend/tailwind.config.js
+++ Shoping-cart-frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Bebas Neue", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
