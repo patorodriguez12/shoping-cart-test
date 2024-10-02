549b151b1071b6e4d52fc82b838494e451610686
patorodriguez12 <pato_rodriguez12@outlook.com>
2024-10-02 10:03:45 -0300

    ✨ feat(cart): implemented cart mockup

--- Shoping-cart-frontend/src/components/Cart.jsx
+++ Shoping-cart-frontend/src/components/Cart.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 function Cart() {
+  const cart = useSelector((state) => state.cart);
   return (
-    <div>Cart</div>
-  )
+    <div>
+      <h2>Shopping Cart</h2>
+      {cart.cartItems.length === 0 ? (
+        <div>
+          <p>Your cart is currently empty</p>
+          <div>
+            <Link to="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                fill="currentColor"
+                className="bi bi-arrow-return-left"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"
+                />
+              </svg>
+              <span>Start Shopping</span>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <div>
+            <h3>Product</h3>
+            <h3>Price</h3>
+            <h3>Quantity</h3>
+            <h3>Total</h3>
+          </div>
+          <div>
+            {cart.cartItems?.map((cartItem) => (
+              <div key={cartItem.id}>
+                <div>
+                  <img src={cartItem.image} alt={cartItem.name} />
+                  <div>
+                    <h3>{cartItem.name}</h3>
+                    <button>Remove</button>
+                  </div>
+                </div>
+                <div>${cartItem.price}</div>
+                <div>
+                  <button>-</button>
+                  <div>{cartItem.cartQuantity}</div>
+                  <button>+</button>
+                </div>
+                <div>${cartItem.price * cartItem.cartQuantity}</div>
+              </div>
+            ))}
+          </div>
+          <div>
+            <button>Clear Cart</button>
+            <div>
+              <div>
+                <span>Subtotal</span>
+                <span>${cart.cartTotalAmount}</span>
+              </div>
+              <p>Taxes and shipping calculated at checkout</p>
+              <button>Check Out</button>
+              <div>
+                <Link to="/">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-arrow-return-left"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5"
+                    />
+                  </svg>
+                  <span>Continue Shopping</span>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
